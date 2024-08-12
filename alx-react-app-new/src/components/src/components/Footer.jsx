f7dcf04b0e8a73ce5fc3b648a05b8df5d52dc8ee
Andrea716 <andreavisagie07@gmail.com>
2024-08-12 13:36:34 +0200

    Delete alx-react-app-new/src/components/src/components directory

--- alx-react-app-new/src/components/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return (
-      <footer style={{ backgroundColor: 'lightgray', padding: '10px', textAlign: 'center' }}>
-        <p>&copy; 2024 My Favorite Cities</p>
-      </footer>
-    );
-  };
-  
-  export default Footer;
--- alx-react-app-new/src/components/src/components/MainContent.jsx
+++
@@ -1,9 +0,0 @@
-const MainContent = () => {
-    return (
-      <main style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
-        <p>Welcome to the main content area!</p>
-      </main>
-    );
-  };
-  
-  export default MainContent;
