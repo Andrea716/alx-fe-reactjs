9b69effe43697092bc67fd8fb9173a4182db6fbb
Andrea716 <andreavisagie07@gmail.com>
2024-08-12 10:49:32 +0200

    Update Footer.jsx

--- alx-react-app/src/components/Footer.jsx
+++ alx-react-app/src/components/Footer.jsx
@@ -1,9 +1,9 @@
 function Footer() {
-    return (
-        <footer>
-            <p>© 2023 City Lovers</p>
-        </footer>
-    );
+  return (
+    <footer style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center', padding: '10px', marginTop: '20px' }}>
+      <p>© 2024 My Favorite Cities</p>
+    </footer>
+  );
 }
 
 export default Footer;
