a3e616101a310e8a7c55194543d7feb39f3552d4
Andrea716 <andreavisagie07@gmail.com>
2024-08-12 14:28:00 +0200

    Create UserContext.js

---
+++ alx-react-app-new/src/components/UserContext.js
@@ -0,0 +1,13 @@
+// Create a Context for user data
+const UserContext = createContext();
+
+// Create a Provider component
+export const UserProvider = ({ children, value }) => {
+  return (
+    <UserContext.Provider value={value}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserContext;
