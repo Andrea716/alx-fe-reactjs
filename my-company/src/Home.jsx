aac25106eeb4f30de0de5be7ac2c1f9819af44d6
Andrea716 <andreavisagie07@gmail.com>
2024-08-14 19:39:26 +0200

    Create Home.jsx

---
+++ my-company/src/Home.jsx
@@ -0,0 +1,17 @@
+function Home() {
+    return (
+      <div style={{
+        padding: '20px',
+        textAlign: 'center',
+        backgroundColor: '#f4f4f4',
+        color: '#333'
+      }}>
+        <h1 style={{ color: '#2c3e50' }}>Welcome to Our Company</h1>
+        <p style={{ fontSize: '18px', lineHeight: '1.6' }}>
+          We are dedicated to delivering excellence in all our services.
+        </p>
+      </div>
+    );
+  }
+  
+  export default Home;
