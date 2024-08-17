f90b6a7041d0136a003a328ec62c50bf24e92086
Andrea716 <andreavisagie07@gmail.com>
2024-08-17 12:58:31 +0200

    Create Footer.jsx

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+      <footer style={{ padding: '10px', backgroundColor: '#f4f4f4', textAlign: 'center' }}>
+        <p>&copy; 2024 My Company</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
