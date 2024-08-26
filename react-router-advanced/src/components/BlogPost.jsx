f9c5e12751a86032df669afb6fc8b6ab30d4d1e1
Andrea716 <andreavisagie07@gmail.com>
2024-08-26 11:32:44 +0200

    Update BlogPost.jsx

--- react-router-advanced/src/components/BlogPost.jsx
+++ react-router-advanced/src/components/BlogPost.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
-const BlogPost = () => {
-  const { id } = useParams();
-  return <h1>Blog Post ID: {id}</h1>;
-};
+function BlogPost() {
+  const { id } = useParams(); // Get the dynamic part of the route
+
+  // You can now use the `id` to fetch or display data related to this blog post
+
+  return (
+    <div>
+      <h1>Blog Post {id}</h1>
+      {/* Render blog post details here */}
+    </div>
+  );
+}
 
 export default BlogPost;
