3cbdef7d22b257a8444a475649d37c2f5aa3ba85
Andrea716 <andreavisagie07@gmail.com>
2024-09-16 10:49:47 +0200

    Update githubServices.js

--- github-user-search/src/services/githubServices.js
+++ github-user-search/src/services/githubServices.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
-const GITHUB_API_URL = 'https://api.github.com';
+const GITHUB_API_URL = 'https://api.github.com/users/';
 
-export const searchUsers = async (username) => {
-  const response = await axios.get(`${GITHUB_API_URL}/search/users?q=${username}`);
-  return response.data;
+export const fetchUserData = async (username) => {
+  try {
+    const response = await axios.get(`${GITHUB_API_URL}${username}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 };
